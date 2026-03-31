b8db142b79553f6d6ba8cf2826f9c9e206705000
Chiara Carpiso <b2029mcmcarpiso@pshs.edu.ph>
2026-03-31 22:36:54 +0800

    3rd graded

--- public/q4/3rdGraded/script.js
+++ public/q4/3rdGraded/script.js
@@ -1,25 +1,34 @@
-function plotPoint() {
-    console.log(x0, y0, x, y) // checks the arguments passed to this function
-    in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
-    var point = document.createElement('div');  
+function plotPoint(x0, y0, x, y) {
+    console.log(x0, y0, x, y); // checks the arguments passed to this function
+    
+    document.getElementById("in1").innerHTML = x0 + " " + y0 + " " + x + " " + y;
+    
+    let point = document.createElement('div');  
     point.className = 'point';
     /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.styleleft = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.stylebottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    
+    point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
+    point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    
     document.getElementById('coordinatePlane').appendChild(point);
 
-    if (x > x0 && y > y0){
-      out1.innerHTML = "NE"
-    }
-    else if (x < x0 && y < y0){
-      out1.innerHTML = "SO"
-    }
-    else if (x > x0 && y < y0){
-      out1.innerHTML = "SE"
+    let result = "";
+
+    if (x === x0 || y === y0){
+      result = "divisa";
+    } 
+    else if (x > x0 && y > y0){
+      result = "NE";
     }
     else if (x < x0 && y > y0){
-      out1.innerHTML = "NO"
+      result = "NO";
     }
-    else{
-      out1.innerHTML = "divisa"
+    else if (x > x0 && y < y0){
+      result = "SE";
     }
+    else {
+      result = "SO";
+    }
+
+    document.getElementById("out1").innerHTML = result;
+}
